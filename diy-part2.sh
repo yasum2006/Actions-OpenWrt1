d4f329160e47874cc5417739f652bcdfa197daba
skywalker <20499386+skywalkker@users.noreply.github.com>
2020-06-08 10:55:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+ git clone https://github.com/skywalkker/passwall.git  package/luci-app-passwall
+  git clone  -b master https://github.com/vernesong/OpenClash.git  package/luci-app-openclash
