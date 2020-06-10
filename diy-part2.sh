cf7d6de7c231e35113b5af7c8e04874e9f97c897
skywalker <20499386+skywalkker@users.noreply.github.com>
2020-06-10 15:00:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,4 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
  #svn co --force https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus  package/luci-app-ssr-plus  && svn revert -R package/luci-app-ssr-plus
 git clone -b master https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus package/luci-app-adbyby-plus   && svn revert -R package/luci-app-adbyby-plus 
+svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd package/luci-app-vlmcsd   && svn revert -R package/luci-app-vlmcsd
