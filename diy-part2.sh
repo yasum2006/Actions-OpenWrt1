cbe3fd08bcf0df8f10a38de58de95f696e81239c
skywalker <20499386+skywalkker@users.noreply.github.com>
2020-06-11 16:26:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,5 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #git clone -b master https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #git clone -b master https://github.com/rufengsuixing/luci-app-zerotier.git package/luci-app-zerotier
 #svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus package/luci-app-adbyby-plus   && svn revert -R package/luci-app-adbyby-plus 
-#svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd package/luci-app-vlmcsd   && svn revert -R package/luci-app-vlmcsd
+#svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean  && svn revert -R package/lean
+
