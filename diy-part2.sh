1550c0934bf420d6fe5fb020869df6675e8dfb38
skywalker <20499386+skywalkker@users.noreply.github.com>
2020-06-13 13:16:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,9 +16,9 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-th
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
-svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus package/luci-app-adbyby-plus   && svn revert -R package/luci-app-adbyby-plus 
+#svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus package/luci-app-adbyby-plus   && svn revert -R package/luci-app-adbyby-plus 
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean//luci-app-zerotier package/luci-app-zerotier   && svn revert -R package/luci-app-zerotier
-svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean//luci-app-vlmcsd package/luci-app-vlmcsd   && svn revert -R package/luci-app-vlmcsd
+#svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean//luci-app-vlmcsd package/luci-app-vlmcsd   && svn revert -R package/luci-app-vlmcsd
  
 #svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean  && svn revert -R package/lean
 
