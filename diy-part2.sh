be3e771a1532371a2c8ccbde7f9c8f0cb26f25e6
skywalker <20499386+skywalkker@users.noreply.github.com>
2020-06-13 16:06:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/skywalkker/passwall.git  package/luci-app-passwall
 #svn co --force https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus  package/luci-app-ssr-plus  && svn revert -R package/luci-app-ssr-plus
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomato t.git package/luci-theme-opentomato
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomato
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
