c65374cb5381d337b41c92f17e555a69c6a9d020
skywalker <20499386+skywalkker@users.noreply.github.com>
2020-06-15 17:11:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
@@ -17,5 +18,5 @@ git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby package/adbyby   && svn revert -R package/adbyby
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus package/luci-app-adbyby-plus   && svn revert -R package/luci-app-adbyby-plus 
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean//luci-app-zerotier package/luci-app-zerotier   && svn revert -R package/luci-app-zerotier
-#svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean package/lean  && svn revert -R package/lean
+
 
