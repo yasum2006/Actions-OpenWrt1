4dd2de2858c9985c8edfc2f33604f579d7723e03
starambler <20499386+starambler@users.noreply.github.com>
2020-12-28 11:14:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,8 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
+git clone https://github.com/fw876/helloworld.git package/helloworld
 
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/luci-app-zerotier && svn revert -R package/luci-app-zerotier
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-softethervpn package/luci-app-softethervpn && svn revert -R package/luci-app-softethervpn
