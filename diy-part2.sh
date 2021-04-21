78ac472c17378ea7f21f92e550def4e051d2ea16
starambler <20499386+starambler@users.noreply.github.com>
2021-04-21 20:50:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,12 +14,13 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #custom your packages
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone  https://github.com/k-szuster/luci-access-control.git package/luci-app-access-control
 
 #svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata package/luci-app-netdata && svn revert -R package/luci-app-netdata
+svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-accesscontrol package/luci-app-accesscontrol && svn revert -R package/luci-app-accesscontrol
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/luci-app-zerotier && svn revert -R package/luci-app-zerotier
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-softethervpn package/luci-app-softethervpn && svn revert -R package/luci-app-softethervpn
 svn co --force https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome && svn revert -R package/luci-app-adguardhome
+
 
 #passwall-package 
 git clone https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
