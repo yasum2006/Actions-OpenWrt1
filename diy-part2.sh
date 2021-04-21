69d9c9ef5ae7666ecb1cc2c2072a38ed4b78e37c
starambler <20499386+starambler@users.noreply.github.com>
2021-04-21 16:00:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #custom your packages
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone  https://github.com/Aslin-Ameng/luci-app-access-control.git package/luci-app-access-control
+git clone  https://github.com/k-szuster/luci-access-control.git package/luci-app-access-control
 
 #svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata package/luci-app-netdata && svn revert -R package/luci-app-netdata
 svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-zerotier package/luci-app-zerotier && svn revert -R package/luci-app-zerotier
