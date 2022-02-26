5da1a61b5fd044f9cc0303fdf3cb6cc6c08abed7
starambler <20499386+starambler@users.noreply.github.com>
2022-02-26 21:50:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git luci https://github.com/openwrt/luci.git' feeds.conf.default
