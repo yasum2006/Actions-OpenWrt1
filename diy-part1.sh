1c6fe2f5cff3ea0cf1f913ac22224c24d5a9e208
starambler <20499386+starambler@users.noreply.github.com>
2022-02-26 22:15:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 
 
 # Uncomment a feed source
-sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
+#sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
