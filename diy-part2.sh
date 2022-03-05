a79166686e3b31254d1267411a10af2129fafe99
starambler <duyouman@gmail.com>
2022-03-05 12:04:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app
 #svn co --force https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome && svn revert -R package/luci-app-adguardhome
 
 #passwall-package 
-git clone https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
+git clone -b packages  https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
 
 #add upx
 mkdir -p tools/ucl && wget -P tools/ucl https://raw.githubusercontent.com/coolsnowwolf/lede/master/tools/ucl/Makefile 
