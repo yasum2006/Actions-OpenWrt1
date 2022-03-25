75a3ace7c639584555d37c86c35960452b5e5b85
starambler <duyouman@gmail.com>
2022-03-25 10:16:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-#custom your packages
+#openclash packages
 git clone -b master  https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 #passwall package
