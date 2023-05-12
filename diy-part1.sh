5603536fb835e90f74969e722a1fef211ce0ee9a
eorendel <duyouman@gmail.com>
2023-05-12 10:35:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,5 +12,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
- sed -i '$a src-git package https://github.com/armorgen/openwrt-luci.git' feeds.conf.default
+ sed -i '$a src-git package https://github.com/eorendel/openwrt-luci.git' feeds.conf.default
  #sed -i '$a src-git package https://github.com/coolsnowwolf/luci' feeds.conf.default
